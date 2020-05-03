53a9c9366ff01bdfc7e3219d233b3b5c4f75715b
Arrettino <batukearrettino@outlook.com>
2020-05-03 19:18:22 -0300

    creat porject

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <p>under construction</p>
+      </div>
+    );
+  }
+}
+
+export default App;
